68691b4b55853255d45237bd024d6f5ca6e7fd20
Aaryan <aryanverma.av2000@gmail.com>
2022-05-03 13:11:56 +0530

    Implemented React Router and added path for homepage and nft list display page

---
+++ src/nftList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./nftList.css"
+function NFTList() {
+  return (
+    <div className='nftlist'>This is the NFT List display page
+        <p>NFT LIST DISPLAY</p>
+    </div>
+  )
+}
+
+export default NFTList
